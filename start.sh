8a076ac73946e8d613759fbaa00f5dc407e676bf
Vince Hillier <vince@revenni.com>
2019-12-28 12:33:43 -0500

    remove locking and move config updates to function

--- start.sh
+++ start.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
-INITIAL_SETUP_LOCK=/taiga-conf/.initial_setup.lock
-if [ ! -f $INITIAL_SETUP_LOCK ]; then
-	touch $INITIAL_SETUP_LOCK
-	[ "$TAIGA_SCHEME" = 'https' ] && TAIGA_WS_SCHEME=wss || TAIGA_WS_SCHEME=ws
-	sed -e 's/$TAIGA_HOST/'$TAIGA_HOST'/' \
-		-e 's/$TAIGA_SCHEME/'$TAIGA_SCHEME'/' \
-		-e 's/$TAIGA_WS_SCHEME/'$TAIGA_WS_SCHEME'/' \
-		-i /tmp/taiga-conf/config.json
-	cp /tmp/taiga-conf/config.json /taiga-conf/
-	ln -sf /taiga-conf/config.json /srv/taiga/front/dist/conf.json
-else
+update_configs() {
+    [ "$TAIGA_SCHEME" = 'https' ] && TAIGA_WS_SCHEME=wss || TAIGA_WS_SCHEME=ws
+    sed -e 's/$TAIGA_HOST/'$TAIGA_HOST'/' \
+        -e 's/$TAIGA_SCHEME/'$TAIGA_SCHEME'/' \
+        -e 's/$TAIGA_WS_SCHEME/'$TAIGA_WS_SCHEME'/' \
+        -i /tmp/taiga-conf/config.json
+
+    cp /tmp/taiga-conf/config.json /taiga-conf/
     ln -sf /taiga-conf/config.json /srv/taiga/front/dist/conf.json
-fi
+}
+
+update_configs
 
 exec nginx -g 'daemon off;'
